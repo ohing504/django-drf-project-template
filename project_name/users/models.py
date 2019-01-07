f472ba2588a3e4a35b38f3bb1c1e94a3cd53c211
Youngsup Oh <ohing504@gmail.com>
2019-01-07 18:17:07 +0900

    Add custom user model manager

--- project_name/users/models.py
+++ project_name/users/models.py
@@ -1,5 +1,9 @@
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, UserManager
+
+
+class CustomUserManager(UserManager):
+    pass
 
 
 class User(AbstractUser):
-    pass
+    objects = CustomUserManager()
