9bcc57596610ac6281842156130dacfd1c22e9e2
Youngsup Oh <ohing504@gmail.com>
2019-05-17 18:13:50 +0900

    Upgrade django to 2.2.1

--- project_name/urls.py
+++ project_name/urls.py
@@ -1,4 +1,20 @@
-from django.conf import settings
+"""project_name URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/{{ docs_version }}/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+import os
+
 from django.contrib import admin
 from django.urls import include, path, re_path
 from drf_yasg import openapi
@@ -35,8 +51,9 @@ urlpatterns = [
     path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
 ]
 
-if settings.DEBUG:
+if os.getenv('ENVIRONMENT', 'development').title() == 'Development':
     import debug_toolbar
+
     urlpatterns = [
-        path('__debug__/', include(debug_toolbar.urls)),
-    ] + urlpatterns
+                      path('__debug__/', include(debug_toolbar.urls)),
+                  ] + urlpatterns
